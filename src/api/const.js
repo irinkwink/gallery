a035a1791363a213be313f5a705605272712d97e
irinkwink <irina-ns@yandex.ru>
2022-09-12 00:27:55 +0300

    fix: change Redirect_Uri

--- src/api/const.js
+++ src/api/const.js
@@ -3,7 +3,7 @@ export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize';
 export const ACCESS_KEY = 'vA4f3GL-ThJ-iKML6DCOlfpYgxTAaVWGn37iAk0h7ao';
 export const SECRET_KEY = 'w5FyGQn-OSTEMr4ruvcrvbDQloWxpYdOLsBCWahAWsc';
-export const REDIRECT_URI = 'http://localhost:3000/';
+export const REDIRECT_URI = 'https://gallery-irinkwink.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
 
